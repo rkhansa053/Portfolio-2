741d6adeacd88b14e283a8117df8cdab3b80f534
rkhansa053 <160234986+rkhansa053@users.noreply.github.com>
2024-11-14 20:40:22 +0500

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: ["img.freepik.com", "encrypted-tbn0.gstatic.com", "www.shutterstock.com", "encrypted-tbn0.gstatic.com", "www.texttool.com", "images.pexels.com"], 
+  },
+};
+
+export default nextConfig;
